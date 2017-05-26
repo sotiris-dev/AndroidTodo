19b3c14782269604eec35969ac82acd3fc24cf17
ubuntu <ubuntu@ubuntu.lan>
2017-05-26 17:14:17 +0300

    Initial commit

---
+++ app/src/main/java/com/todo/androidtodo/Login.java
@@ -0,0 +1,71 @@
+package com.todo.androidtodo;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    FirebaseDatabase fdb;
+    DatabaseReference fref;
+    FirebaseAuth auth;
+    Button btnlogin;
+    Button btnregister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+        btnlogin = (Button) findViewById(R.id.Button_login);
+        btnregister = (Button) findViewById(R.id.Button_Register);
+
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = (EditText) findViewById(R.id.Email_login);
+                EditText pass = (EditText) findViewById(R.id.Password_login);
+                auth = FirebaseAuth.getInstance();
+                auth.signInWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //redirect to main app
+                    }
+                });
+            }
+        });
+
+
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  aint = new Intent(Login.this, Register.class);
+                startActivity(aint);
+            }
+        });
+    }
+}
